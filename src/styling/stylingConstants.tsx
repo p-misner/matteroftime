6c9af5535fb9c52a13511b2dee20b1e4e033e5bc
Priya Misner <priyamisner@gmail.com>
2023-01-08 17:21:49 -0500

    creating vizColor constants

--- src/styling/stylingConstants.tsx
+++ src/styling/stylingConstants.tsx
@@ -5,4 +5,21 @@ export const tabletBreakpoint = "768px";
 export const maxWidth = "1170px";
 export const h2 = "24px";
 export const heroText = "32px";
+
+/* Colors */
 export const fontColor = "#000";
+// export const yellow = "#F9D147";
+// export const brightGreen = "#9BE400";
+// export const adamantineBlue = "#4BADF8";
+// export const pastelPurple = "#9980FA";
+// export const pink = "#FA80D8";
+// export const neonSeaFoam = "#1ADB9A";
+
+export const vizColors = {
+  yellow: "#F9D147",
+  brightGreen: "#9BE400",
+  adamantineBlue: "#4BADF8",
+  pastelPurple: "#9980FA",
+  pink: "#FA80D8",
+  neonSeaFoam: "#1ADB9A",
+};
